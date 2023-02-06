fa7bfd5947572b8981324e74a472ee7788fb6f53
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2023-02-06 19:14:49 +0500

    lesson5

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,8 +1,17 @@
 <template>
-    <h1>Products home page</h1>
+    <div>
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="(product) in products"  :key="product.title">
+                <NuxtLink>{{product.title}} </NuxtLink>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
+
+const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
     definePageMeta({
         layout: 'products'
     })
